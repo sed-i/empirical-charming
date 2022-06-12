187246726511d6ec21033a1e33be594b195dd08d
sed-i <82407168+sed-i@users.noreply.github.com>
2022-06-12 00:15:55 -0400

    [bare] Print hook name

--- bare/src/charm.py
+++ bare/src/charm.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     # /var/lib/juju/tools/unit-bare-0/ is already in the PATH, so can call hooks without full path.
     # (os.environ is inherited to the callee.)
     if hook_name := os.environ.get("JUJU_HOOK_NAME"):
-        call(["juju-log", "-l", "INFO", hook_name])
+        call(["juju-log", "-l", "INFO", f"Hook: {hook_name}"])
     else:
         call(["juju-log", "-l", "ERROR", "This is odd: JUJU_HOOK_NAME is not set!"])
     call(["status-set", "active", "Woohoo!"])
