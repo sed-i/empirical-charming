c84fb3eeab04519ce7d14efd6ef47fd936e9648a
sed-i <82407168+sed-i@users.noreply.github.com>
2022-10-13 00:57:17 +0300

    Add custom hook sample

--- charming-in-rust/hook_printer/src/main.rs
+++ charming-in-rust/hook_printer/src/main.rs
@@ -5,7 +5,15 @@ mod jujuenv;
 use jujuenv::parse_hook_context;
 
 
+fn on_install() {
+    println!("Custom on_install hook");
+}
 
 fn main() {
-    println!("Hello Juju: {:?}", parse_hook_context());
+    let ctx = parse_hook_context();
+    match ctx {
+        jujuenv::HookContext::Install(_, _) => on_install(),
+        _ => {},
+    }
+    println!("Hello Juju: {:?}", ctx);
 }
